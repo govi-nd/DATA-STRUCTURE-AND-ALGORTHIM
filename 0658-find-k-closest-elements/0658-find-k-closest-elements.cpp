cdeb9f95919bd8186b7bb2f4eafe1ec0f78d707f
Govind <gprashar2005@gmail.com>
2026-07-12 11:49:11 +0530

    Time: 2 ms (57.23%), Space: 35.9 MB (80.28%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,46 +1,59 @@
 class Solution {
 public:
+    // vector<int> twoPtr(vector<int>& arr, int k, int x) {
+    //     int l = 0;
+    //     int h = arr.size() - 1;
+    //     while ((h - l) >= k) {
+    //         if (x - arr[l] > arr[h] - x) {
+    //             l++;
+    //         } else {
+    //             h--;
+    //         }
+    //     }
+    //     vector<int> ans;
+    //     for (int i = l; i <= h; i++) {
+    //         ans.push_back(arr[i]);
+    //     }
+    //     return ans;
+    // }
     int lowerBound(vector<int>& arr, int x) {
-        int l = 0, h = arr.size() - 1;
-        int ans = arr.size();
+    int l = 0, h = arr.size() - 1;
+    int ans = arr.size();  // default (if all < x)
 
-        while (l <= h) {
-            int mid = l + (h - l) / 2;
-
-            if (arr[mid] >= x) {
-                ans = mid;
-                h = mid - 1;
-            } else {
-                l = mid + 1;
-            }
+    while (l <= h) {
+        int mid = (l + h) / 2;
+        if (arr[mid] >= x) {
+            ans = mid;
+            h = mid - 1;
+        } else {
+            l = mid + 1;
         }
-
-        return ans;
     }
-
+    return ans;
+}
     vector<int> bs_approach(vector<int>& arr, int k, int x) {
-        int n = arr.size();
 
-        // First index having value >= x
-        int h = lowerBound(arr, x);
-        int l = h - 1;
+    int n = arr.size();
 
-        while (k--) {
-            if (l < 0) {
-                h++;
-            } else if (h >= n) {
-                l--;
-            } else if (x - arr[l] > arr[h] - x) {
-                h++;
-            } else {
-                l--;
-            }
+    // find first index >= x
+    int h = lowerBound(arr,x);
+    int l = h - 1;
+
+    while (k--) {
+        if (l < 0) {
+            h++;
+        } else if (h >= n) {
+            l--;
+        } else if (x - arr[l] > arr[h] - x) {
+            h++;
+        } else {
+            l--;
         }
-
-        return vector<int>(arr.begin() + l + 1, arr.begin() + h);
     }
-
+    return vector<int>(arr.begin() + l + 1, arr.begin() + h);
+}
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return bs_approach(arr, k, x);
+        // return twoPtr(arr,k,x);
+        return bs_approach(arr,k,x);
     }
 };
