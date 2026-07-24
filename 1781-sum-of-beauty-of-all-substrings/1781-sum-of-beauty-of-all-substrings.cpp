fd11dc4d31b2e9d62b122d248d19168ddfebdfac
Govind <gprashar2005@gmail.com>
2026-07-24 11:11:38 +0530

    Time: 111 ms (59.87%), Space: 8.9 MB (93.48%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     int beautySum(string s) {
-        unordered_map<char, int> mp;
-        int beautySum = 0;
-        for (int i = 0; i < s.size(); i++) {
-            mp.clear();
-            for (int j = i; j < s.size(); j++) {
-                char& ch = s[j];
-                mp[ch]++;
-                int maxi = INT_MIN;
-                int mini = INT_MAX;
-                for (auto it : mp) {
-                    maxi = max(maxi, it.second);
-                    mini = min(mini, it.second);
+        // unordered_map<char, int> mp;
+        int n = s.size();
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            int freq[26] = {0};
+
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+
+                int mx = 0;
+                int mn = INT_MAX;
+
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] == 0)
+                        continue;
+
+                    mx = max(mx, freq[k]);
+                    mn = min(mn, freq[k]);
                 }
-                int sum = maxi - mini;
-                beautySum += sum;
+
+                ans += mx - mn;
             }
         }
-        return beautySum;
+        // for (int i = 0; i < s.size(); i++) {
+        //     mp.clear();
+        //     for (int j = i; j < s.size(); j++) {
+        //         char& ch = s[j];
+        //         mp[ch]++;
+        //         int maxi = INT_MIN;
+        //         int mini = INT_MAX;
+        //         for (auto it : mp) {
+        //             maxi = max(maxi, it.second);
+        //             mini = min(mini, it.second);
+        //         }
+        //         int sum = maxi - mini;
+        //         beautySum += sum;
+        //     }
+        // }
+        return ans;
     }
 };
