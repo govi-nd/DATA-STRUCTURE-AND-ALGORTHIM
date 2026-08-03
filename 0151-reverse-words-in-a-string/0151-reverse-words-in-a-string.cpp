974fd16a99e9a4b1393d12f651343c729cc22c57
Govind <gprashar2005@gmail.com>
2026-08-03 22:07:58 +0530

    Time: 3 ms (34.12%), Space: 9.9 MB (95.21%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,91 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int n = s.length();
+
+        // Reverse the entire string
+        int i = 0;
+        int j = n - 1;
+
+        while (i < j) {
+            swap(s[i], s[j]);
+            i++;
+            j--;
+        }
+
+        // Reverse each word
+        int x = 0;
+        while (x < n) {
+
+            // Skip spaces
+            while (x < n && s[x] == ' ')
+                x++;
+
+            if (x >= n)
+                break;
+
+            int z = x;
+
+            // Find end of current word
+            while (z < n && s[z] != ' ')
+                z++;
+
+            // Reverse the word
+            reverse(s.begin() + x, s.begin() + z);
+
+            x = z;
+        }
+
+        int write = 0;
+        int read = 0;
+
+        while (read < n) {
+
+            // Skip all spaces
+            while (read < n && s[read] == ' ')
+                read++;
+
+            if (read >= n)
+                break;
+
+            // Put one space before every word except the first
+            if (write != 0)
+                s[write++] = ' ';
+
+            // Copy the current word
+            while (read < n && s[read] != ' ')
+                s[write++] = s[read++];
+        }
+
+        s.resize(write);
+        return s;
+        // int n = s.length();
+        // vector<string> str;
+
+        // for (int i = 0; i < n; i++) {
+        //     if (s[i] == ' ')
+        //         continue;
+        //     else {
+        //         int j = i;
+
+        //         while (j < n && s[j] != ' ') {
+        //             j++;
+        //         }
+
+        //         str.push_back(s.substr(i, j - i));
+        //         i = j - 1;   // because the for loop will increment i
+        //     }
+        // }
+
+        // string ans;
+
+        // for (int i = str.size() - 1; i >= 0; i--) {
+        //     ans += str[i];
+
+        //     if (i != 0)
+        //         ans += ' ';
+        // }
+
+        // return ans;
+    }
+};
