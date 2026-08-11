0da12dac93ac1b7cd6e516ec395e2ddedf5e8022
Govind <gprashar2005@gmail.com>
2026-08-11 16:17:15 +0530

    Time: 0 ms (100%), Space: 10 MB (64.92%) - LeetHub

--- 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
+++ 0876-middle-of-the-linked-list/0876-middle-of-the-linked-list.cpp
@@ -11,22 +11,33 @@
 class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
-        ListNode* temp = head;
-        int count = 0;
-        while (temp != NULL) {
-            count++;
-            temp = temp->next;
+
+        ListNode* kachuva = head;
+        ListNode* rabbit = head;
+
+        while (rabbit != NULL && rabbit->next != NULL) {
+            kachuva = kachuva->next;
+            rabbit = rabbit->next->next;
         }
-        int pos = count / 2;
-        // cout<<pos<<endl;
-        ListNode* temp2 = head;
-        for (int i = 1; i < pos + 1; i++) {
-            // cout << i << " " << temp2->val << " "<<endl;
-            temp2 = temp2->next;
-            
-        }
-        // cout<<endl;
-        // cout<<"temp->stopped at"<<temp2->val<<endl;
-        return temp2;
+
+        return kachuva;
+
+        // ListNode* temp = head;
+        // int count = 0;
+        // while (temp != NULL) {
+        //     count++;
+        //     temp = temp->next;
+        // }
+        // int pos = count / 2;
+        // // cout<<pos<<endl;
+        // ListNode* temp2 = head;
+        // for (int i = 1; i < pos + 1; i++) {
+        //     // cout << i << " " << temp2->val << " "<<endl;
+        //     temp2 = temp2->next;
+
+        // }
+        // // cout<<endl;
+        // // cout<<"temp->stopped at"<<temp2->val<<endl;
+        // return temp2;
     }
 };
