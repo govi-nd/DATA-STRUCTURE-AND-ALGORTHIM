afcef109ea4665e0717cf2a98906356c0c381503
Govind <gprashar2005@gmail.com>
2026-08-20 15:32:27 +0530

    Time: 8 ms (5.84%), Space: 263.5 MB (8.96%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -0,0 +1,26 @@
+// class Solution {
+// public:
+//     void rotate(vector<int>& nums, int k) {
+
+//         int n = nums.size();
+//         k %= n;
+
+//         vector<int> ans(n);
+
+//         for (int i = 0; i < n; i++)
+//             ans[(i + k) % n] = nums[i];
+
+//         nums = ans;
+//     }
+// };
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        k %= n;
+
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
+    }
+};
