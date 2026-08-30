2639cc003c2e9e4f92fd30a4db4f6aa0c0b145f9
Govind <gprashar2005@gmail.com>
2026-08-30 15:54:25 +0530

    Time: 23 ms (51.36%), Space: 108.3 MB (9.29%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,31 +1,98 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
         stack<int> s;
         vector<int> ans;
+
         for (int i = temperatures.size() - 1; i >= 0; i--) {
+
             while (!s.empty() && temperatures[s.top()] <= temperatures[i]) {
                 s.pop();
             }
+
             if (s.empty()) {
                 ans.push_back(-1);
-            } else {
-                ans.push_back(s.top());
+            }
+            else {
+                ans.push_back(s.top() - i);
             }
 
             s.push(i);
         }
-        reverse(ans.begin(),ans.end());
 
+        reverse(ans.begin(), ans.end());
+
+        // Convert -1 to 0
         for (int i = 0; i < ans.size(); i++) {
             if (ans[i] == -1) {
                 ans[i] = 0;
-            } else {
-                int index = ans[i];
-                int days = index - i;
-                ans[i] = days;
             }
         }
+
         return ans;
     }
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         stack<int> s;
+//         vector<int> ans;
+//         for (int i = temperatures.size() - 1; i >= 0; i--) {
+//             while (!s.empty() && temperatures[s.top()] <= temperatures[i]) {
+//                 s.pop();
+//             }
+//             if (s.empty()) {
+//                 ans.push_back(-1);
+//             } else {
+//                 ans.push_back(s.top());
+//             }
+
+//             s.push(i);
+//         }
+//         reverse(ans.begin(),ans.end());
+
+//         for (int i = 0; i < ans.size(); i++) {
+//             if (ans[i] == -1) {
+//                 ans[i] = 0;
+//             } else {
+//                 int index = ans[i];
+//                 int days = index - i;
+//                 ans[i] = days;
+//             }
+//         }
+//         return ans;
+//     }
+// };
