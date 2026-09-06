5bfd25b4c647a6c3fa4ec04d8086c1dc006daf0c
Govind <gprashar2005@gmail.com>
2026-09-06 17:11:33 +0530

    Time: 0 ms (100%), Space: 14.9 MB (94.94%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,41 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
 
         vector<vector<int>> ans;
 
-        if (root == NULL)
+        if (!root)
             return ans;
+
+        bool leftToRight = true;
 
         queue<TreeNode*> q;
         q.push(root);
 
-        bool leftToRight = true;
-
         while (!q.empty()) {
 
-            int n = q.size();
-            vector<int> path;
+            int width = q.size();
+            vector<int> level(width);
 
-            for (int i = 0; i < n; i++) {
+            for (int i = 0; i < width; i++) {
 
-                TreeNode* node = q.front();
+                TreeNode* front = q.front();
                 q.pop();
 
-                path.push_back(node->val);
+                int index = leftToRight ? i : width - i - 1;
 
-                if (node->left)
-                    q.push(node->left);
+                level[index] = front->val;
 
-                if (node->right)
-                    q.push(node->right);
+                if (front->left)
+                    q.push(front->left);
+
+                if (front->right)
+                    q.push(front->right);
             }
 
-            if (!leftToRight)
-                reverse(path.begin(), path.end());
-
-            ans.push_back(path);
-
+            ans.push_back(level);
             leftToRight = !leftToRight;
         }
 
