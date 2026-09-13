e9d0623ea3cd96defed14cb24227a89bee0b53d7
Govind <gprashar2005@gmail.com>
2026-09-13 12:19:02 +0530

    Time: 0 ms (100%), Space: 146.8 MB (51.17%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.cpp
@@ -6,28 +6,63 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode* root , int path , int &ans){
-        if(root==NULL) return;
-        path++;
-        if(root->left == NULL && root->right==NULL){
-            ans = min(ans,path);
-            path--;
-        }
-        solve(root->left,path,ans);
-        solve(root->right,path,ans);
-        path--;
-
-    }
     int minDepth(TreeNode* root) {
-        if(root==NULL) return 0;
-        int ans = INT_MAX;
-        int path = 0 ;
-        solve(root,path,ans);
-        return ans;
+        if (root == NULL)
+            return 0;
+
+        queue<TreeNode*> q;
+        q.push(root);
+        q.push(NULL);
+
+        int depth = 1;
+
+        while (!q.empty()) {
+            TreeNode* node = q.front();
+            q.pop();
+
+            if (node == NULL) {
+                depth++;
+                if (!q.empty())
+                    q.push(NULL);
+            } else {
+
+                if (node->left == NULL && node->right == NULL)
+                    return depth;
+
+                if (node->left)
+                    q.push(node->left);
+
+                if (node->right)
+                    q.push(node->right);
+            }
+        }
+
+        return depth;
     }
+
+    // void solve(TreeNode* root , int path , int &ans){
+    //     if(root==NULL) return;
+    //     path++;
+    //     if(root->left == NULL && root->right==NULL){
+    //         ans = min(ans,path);
+    //         path--;
+    //     }
+    //     solve(root->left,path,ans);
+    //     solve(root->right,path,ans);
+    //     path--;
+
+    // }
+    // int minDepth(TreeNode* root) {
+    //     if(root==NULL) return 0;
+    //     int ans = INT_MAX;
+    //     int path = 0 ;
+    //     solve(root,path,ans);
+    //     return ans;
+    // }
 };
